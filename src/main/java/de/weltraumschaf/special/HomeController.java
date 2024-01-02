943b0f175c0076e91ace268e698ad20550561256
Sven Strittmatter <ich@weltraumschaf.de>
2024-01-02 22:57:25 +0100

    List some entries
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- src/main/java/de/weltraumschaf/special/HomeController.java
+++ src/main/java/de/weltraumschaf/special/HomeController.java
@@ -1,5 +1,11 @@
 package de.weltraumschaf.special;
 
+import com.hierynomus.msfscc.fileinformation.FileIdBothDirectoryInformation;
+import com.hierynomus.smbj.SMBClient;
+import com.hierynomus.smbj.auth.AuthenticationContext;
+import com.hierynomus.smbj.connection.Connection;
+import com.hierynomus.smbj.session.Session;
+import com.hierynomus.smbj.share.DiskShare;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
@@ -8,6 +14,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 @Controller
 final class HomeController {
@@ -25,9 +34,28 @@ final class HomeController {
     }
 
     @PostMapping(value = "/", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
-    public ModelAndView connect(ConnectRequest formData) {
+    public ModelAndView connect(ConnectRequest formData) throws IOException {
+        final var authCtx = new AuthenticationContext(
+            formData.getUsername(),
+            formData.getPassword().toCharArray(),
+            formData.getDomain());
+
+        final var fileEntries = new ArrayList<String>();
+
+        try (final var client = new SMBClient();
+             final var connection = client.connect(formData.getServer())) {
+            final var session = connection.authenticate(authCtx);
+
+            // Connect to Share
+            try (final var share = (DiskShare) session.connectShare(formData.getShare())) {
+                for (FileIdBothDirectoryInformation f : share.list("sxs")) {
+                    fileEntries.add(f.getFileName());
+                }
+            }
+        }
+
         return createModelAndView("home/connect")
-            .addObject("formData", formData);
+            .addObject("fileEntries", fileEntries);
     }
 
     private ModelAndView createModelAndView(String viewName) {
