48485e2177fda721fd486a04179866be18885678
Sven Strittmatter <ich@weltraumschaf.de>
2024-01-02 22:35:22 +0100

    Implement form submit for connection data
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

---
+++ src/main/java/de/weltraumschaf/special/ConnectRequest.java
@@ -0,0 +1,16 @@
+package de.weltraumschaf.special;
+
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+final class ConnectRequest {
+      String username;
+      @ToString.Exclude
+      String password;
+      String server;
+      String domain;
+      String share;
+}
--- src/main/java/de/weltraumschaf/special/HomeController.java
+++ src/main/java/de/weltraumschaf/special/HomeController.java
@@ -3,8 +3,10 @@ package de.weltraumschaf.special;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -20,6 +22,12 @@ final class HomeController {
     @GetMapping(value = "/")
     public ModelAndView home() {
         return createModelAndView("home/index");
+    }
+
+    @PostMapping(value = "/", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    public ModelAndView connect(ConnectRequest formData) {
+        return createModelAndView("home/connect")
+            .addObject("formData", formData);
     }
 
     private ModelAndView createModelAndView(String viewName) {
