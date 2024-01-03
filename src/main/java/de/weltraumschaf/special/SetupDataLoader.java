e212cd406d1f8015d82552286cfb3d0ae8869b26
Sven Strittmatter <ich@weltraumschaf.de>
2024-01-03 23:20:52 +0100

    Add Liqubase setup stuff (didn't work yet)
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- src/main/java/de/weltraumschaf/special/SetupDataLoader.java
+++ src/main/java/de/weltraumschaf/special/SetupDataLoader.java
@@ -3,40 +3,51 @@ package de.weltraumschaf.special;
 import de.weltraumschaf.special.entity.User;
 import de.weltraumschaf.special.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
-    boolean alreadySetup = false;
+    /**
+     * Event may be triggered multiple times, so ensure only one run.
+     */
+    private boolean alreadySetup = false;
 
-    private final UserRepository userRepository;
+    private final UserRepository users;
 
-    private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder encoder;
 
     @Autowired
-    public SetupDataLoader(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public SetupDataLoader(@NonNull UserRepository users, @NonNull PasswordEncoder encoder) {
         super();
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+        this.users = users;
+        this.encoder = encoder;
     }
 
     @Override
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         if (alreadySetup) {
+            log.info("Setup data loader already executed. Skipping!");
             return;
         }
 
+        if (users.findByUsername("sxs").isPresent()) {
+            log.info("Test user already exists. Skipping!");
+            return;
+        }
 
         final var user = new User();
         user.setUsername("sxs");
-        user.setPassword(passwordEncoder.encode("test1234"));
-        userRepository.save(user);
+        user.setPassword(encoder.encode("test1234"));
+        users.save(user);
 
         alreadySetup = true;
     }
